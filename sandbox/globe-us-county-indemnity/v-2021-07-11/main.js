362b95b2910da7e514e56620982e8fc09e98e97d
Theo Armour <t.armour@gmail.com>
2021-07-11 13:12:09 -0700

    update readme

--- sandbox/globe-us-county-indemnity/v-2021-07-11/main.js
+++ sandbox/globe-us-county-indemnity/v-2021-07-11/main.js
@@ -6,7 +6,7 @@ const COR = {
 	defaultFile: "README.md",
 	ignoreFolders: [],
 	filterFiles: [ "gif", "md", "jpg", "license", "pdf", "png", "svg", "txt" ],
-	urlSource: "https://github.com/pushme-pullyou/tootoo-2021/tree/main/lib-geo/glc-globe-csv",
+	urlSource: "https://github.com/theo-armour/maps-2021/tree/main/sandbox/globe-us-county-indemnity",
 	urlAssets: "https://pushme-pullyou.github.io/tootoo-2021/",
 	iconGitHub: `<img src="https://pushme-pullyou.github.io/tootoo-2021/lib/assets/icons/mark-github.svg">`,
 	iconInfo: `<img class=infoImg src="https://pushme-pullyou.github.io/tootoo-2021/lib/assets/icons/noun_Information_585560.svg">`,
@@ -19,16 +19,14 @@ const COR = {
 
 function init () {
 
-	//COR.path = COR.assets;
-	//COR.assets = COR.path;
-
 	MNU.init();
+
+	MNUspnVersion.hidden = true;
 
 	selYear.innerHTML = "<option>Total ( 1/10 scale of other)</option>";
 
-	//MNUdivSample.innerHTML = MNU.getSample();
 
-	for ( var i = 0; i < 39; i++ ) {
+	for ( let i = 0; i < 39; i++ ) {
 
 		selYear.innerHTML += `<option>${ 2017 - i }</option>`;
 
@@ -38,47 +36,14 @@ function init () {
 
 	selYear.focus();
 
-
-	//const htm = "Hello, World!";
-	//MNU.init( { description: htm } );
-	//MNUaSource.hidden = true;
-	//MNUspnVersion.hidden = true;
-
-	// FOX = File Open XHR
-	//FOX.init();
-	//FOXdivDetails.innerHTML = FOX.getMenu();
-	//FOX.onHashChange();
-
-	// GOR.init();
-	// GORinpUser.value = COR.user + "/" + COR.repo;
-	// GOR.getOrganization();
-	//GORdet.open = true;
-
-	//GRV.getFiles = GRV.getFilesAll;
-	//GRV.getFiles = GRV.getFilesCurated;
-	//GRV.init();
-	//GRVdet.open = true;
-	//GRVsumRepo.hidden = true;
-	//GRV.getRepo();
-
-	//setTimeout( () => { GRVdet253.open = true }, 200 );
-
 	THR.init();
 
 	THR.animate();
 
 	THR.addLights();
 
-	//THR.addGround();
-
 	THR.group = THR.getGroupNew();
 
-//	THRU.addMeshes( 100 );
-
-	// geometry = new THREE.SphereGeometry( 50, 50, 25 );
-	// material = new THREE.MeshNormalMaterial();
-	// mesh = new THREE.Mesh( geometry, material );
-	// THR.group.add( mesh );
 
 	GLO.initGlobeWithBitmap();
 
@@ -92,15 +57,11 @@ function init () {
 
 	THR.camera.position.set( -20, -65, 60 )
 
-	JFC.url = "https://pushme-pullyou.github.io/tootoo-2021/data/simplemaps/worldcities.csv";
 	JFC.url = "https://theo-armour.github.io/maps-2021/data/soil-carbon-coalition/indemnity-geodata.csv";
 
 	JFC.requestFile( JFC.url, JFC.onLoadCsv, JFConParseCsv );
 
 	GLC.init();
-
-	// HRT.init();
-
 
 
 	CORdivStats.innerHTML = `
@@ -127,11 +88,7 @@ function init () {
 
 function JFConParseCsv ( index = 1) {
 
-	//console.log( "", 23 );
-
 	//console.log( "JFC.json", JFC.json );
-
-	//barData = JFC.json.map( line => [ line[ 9 ], line[ 2 ], line[ 3 ] ] );
 
 	THR.group.remove( GLC.group );
 
@@ -156,13 +113,13 @@ function JFConParseCsv ( index = 1) {
 
 }
 
+// replace default RAY,getHTM
 
 RAY.getHtm = function ( intersected ) {
 	//console.log( "main intersected", intersected.instanceId );
 
 	const county = JFC.json[ intersected.instanceId ];
 	//console.log( "county", county);
-	// const mesh = RAY.intersected.object;
 
 	const htm = `
 	<div>
@@ -175,4 +132,5 @@ RAY.getHtm = function ( intersected ) {
 	</div>`;
 
 	return htm;
+
 };
